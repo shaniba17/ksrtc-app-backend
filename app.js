f42a83957c0d7c9da73bfbacf752134a41e0505f
shaniba17 <shanibatm2002@gmail.com>
2024-06-14 05:04:31 +0530

    LogIn added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose=require("mongoose")
 const cors=require("cors")
 const {busmodel}=require("./models/SignUp")
 const bcrypt=require("bcryptjs")
+const jwt =require("jsonwebtoken")
 
 mongoose.connect("mongodb+srv://shanibatm17:shaniba17tm@cluster0.h4a3e.mongodb.net/busdb?retryWrites=true&w=majority&appName=Cluster0")
 
@@ -29,6 +30,38 @@ app.post("/signup",async(req,res)=>
     res.json({"status":"SignUp"})
     }
 })
+
+
+app.post("/login",(req,res)=>{
+    let input =req.body
+    busmodel.find({"emailid":req.body.emailid}).then(
+        (response)=>{
+            if(response.length>0)
+                {
+                    let dbPassword=response[0].password
+                    console.log(dbPassword)
+                    bcrypt.compare(input.password,dbPassword,(error,isMatch)=>{
+                        if (isMatch) {
+                            jwt.sign({email:input.emailid},"bus-app",{expiresIn:"2d"},
+                                (error,token)=>{
+                                if (error) {
+                                    res.json({"status":"unable to create token"})
+                                } else {
+                                    res.json({"status":"success","userid":response[0]._id,"token":token})
+                                }
+                            })
+                        } else {
+                            res.json({"status":"incorrect password"})
+                        }
+                    })
+                }
+            else{
+                res.json({"status":"user not found"})
+            }
+        }
+    )
+    })
+    
 
 app.listen(8080,()=>
 {
